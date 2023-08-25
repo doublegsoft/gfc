3921eb5085db6ecbd550691c9860a8cd3b7e96aa
Christian Gann <guo.guo.gan@gmail.com>
2023-08-25 21:23:38 +0800

    simple gc

--- src/gfc_gc.c
+++ src/gfc_gc.c
@@ -134,6 +134,7 @@ gfc_gc_malloc(size_t size, size_t len)
   }
   size_t total = len * size;
   void* ret = malloc(total);
+  if (1) return ret;
   if (ret == NULL)
   {
     fprintf(stderr, "error to allocate memory in file (%s) at line number (%d).\n", __FILE__, __LINE__);
@@ -244,6 +245,7 @@ gfc_gc_realloc(void* ptr, size_t size, size_t len)
 
   size_t total = len * size;
   void* ret = realloc(ptr, total);
+  if (1) return ret;
 
   int i = 0;
   for (i = 0; i < gc_ctx->len; i++)
@@ -285,6 +287,8 @@ gfc_gc_free(void* ptr)
 {
   if (ptr == NULL)
     return GFC_GC_IS_NULL;
+  free(ptr);
+  if (1) return GFC_GC_OK;
 
   int i = 0;
   for (i = 0; i < gc_ctx->len; i++)
