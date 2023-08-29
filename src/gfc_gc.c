b0716f4c576e2d881dc19ddbd09dd86be9c5c937
Christian Gann <guo.guo.gan@gmail.com>
2023-08-29 14:59:13 +0800

    removed gc unused methods

--- src/gfc_gc.c
+++ src/gfc_gc.c
@@ -353,35 +353,35 @@ gfc_gc_free(void* ptr)
 //}
 
 
-// can fail to produce an aligned result if alignment does not divide 2 * size_t
-int
-gfc_gc_memalign(void **memptr, size_t alignment, size_t size)
-{
-  int(*libc_posix_memalign)(void **, size_t, size_t) = dlsym(RTLD_NEXT, "posix_memalign");
-  size_t offset = (sizeof(size_t) + sizeof(gfc_alloc_cookie) );
-  void * answerplus;
-  int ret = libc_posix_memalign(&answerplus,alignment, size + offset);
-  if(ret) return ret;// nothing can be done
-  gfc_malloced_memeory += size;
-  memcpy(answerplus ,&gfc_alloc_cookie,sizeof(gfc_alloc_cookie));
-  memcpy((char *) answerplus + sizeof(gfc_alloc_cookie),&size,sizeof(size));
-  *memptr = (char *) answerplus + offset;
-  return ret;
-}
-
-void*
-gfc_gc_calloc(size_t count, size_t size) {
-  size_t sz = count * size;
-  void *(*libc_malloc)(size_t) = dlsym(RTLD_NEXT, "malloc");
-  size_t volume = sz + sizeof(size_t) + sizeof(gfc_alloc_cookie);
-  void * answerplus =  libc_malloc(volume);
-  memset(answerplus, 0, volume);
-  if(answerplus == NULL) return answerplus;// nothing can be done
-  gfc_malloced_memeory += sz;
-  memcpy(answerplus ,&gfc_alloc_cookie,sizeof(gfc_alloc_cookie));
-  memcpy((char *) answerplus + sizeof(gfc_alloc_cookie),&sz,sizeof(sz));
-  return ((char *) answerplus) + sizeof(size_t) + sizeof(gfc_alloc_cookie);
-}
+//// can fail to produce an aligned result if alignment does not divide 2 * size_t
+//int
+//gfc_gc_memalign(void **memptr, size_t alignment, size_t size)
+//{
+//  int(*libc_posix_memalign)(void **, size_t, size_t) = dlsym(RTLD_NEXT, "posix_memalign");
+//  size_t offset = (sizeof(size_t) + sizeof(gfc_alloc_cookie) );
+//  void * answerplus;
+//  int ret = libc_posix_memalign(&answerplus,alignment, size + offset);
+//  if(ret) return ret;// nothing can be done
+//  gfc_malloced_memeory += size;
+//  memcpy(answerplus ,&gfc_alloc_cookie,sizeof(gfc_alloc_cookie));
+//  memcpy((char *) answerplus + sizeof(gfc_alloc_cookie),&size,sizeof(size));
+//  *memptr = (char *) answerplus + offset;
+//  return ret;
+//}
+//
+//void*
+//gfc_gc_calloc(size_t count, size_t size) {
+//  size_t sz = count * size;
+//  void *(*libc_malloc)(size_t) = dlsym(RTLD_NEXT, "malloc");
+//  size_t volume = sz + sizeof(size_t) + sizeof(gfc_alloc_cookie);
+//  void * answerplus =  libc_malloc(volume);
+//  memset(answerplus, 0, volume);
+//  if(answerplus == NULL) return answerplus;// nothing can be done
+//  gfc_malloced_memeory += sz;
+//  memcpy(answerplus ,&gfc_alloc_cookie,sizeof(gfc_alloc_cookie));
+//  memcpy((char *) answerplus + sizeof(gfc_alloc_cookie),&sz,sizeof(sz));
+//  return ((char *) answerplus) + sizeof(size_t) + sizeof(gfc_alloc_cookie);
+//}
 
 //void
 //gfc_gc_free(void* p)
